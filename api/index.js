0fb3a90527cd3b88f870a9715622e35f0ed503e3
JUAN-commits222 <juanmolina101076@gmail.com>
2025-12-10 07:24:40 -0500

    corrigiendo y agregando archivos para el despliegue correcto en vercel

---
+++ api/index.js
@@ -0,0 +1,6 @@
+// Archivo: /api/index.js (Entrypoint para Vercel)
+// Este archivo es el punto de entrada que Vercel busca por defecto en la ruta /api
+
+const app = require('../backend/js/app'); // Asegúrate de que esta ruta sea correcta
+
+module.exports = app;
